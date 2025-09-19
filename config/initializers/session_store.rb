77f16d0413c90e17949a7c1f73f68d88f92dc3c2
TheOriginalJiozx <omal0001@stud.kea.dk>
2025-09-19 19:24:05 +0200

    added azure db info

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
-# config/initializers/session_store.rb
 Rails.application.config.session_store :cookie_store,
   key: "_openapi_session",
-  same_site: :lax,   # :lax allows fetch with credentials
-  secure: Rails.env.production?  # secure in prod
+  same_site: :lax,
+  secure: Rails.env.production?
