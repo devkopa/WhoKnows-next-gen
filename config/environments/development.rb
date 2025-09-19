23655256c10c58cf2ee3a81e9dbedefedf65c8d1
TheOriginalJiozx <omal0001@stud.kea.dk>
2025-09-19 19:35:21 +0200

    added web_console permission

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.web_console.permissions = "217.71.1.167"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
