c80b6f60b20d3781489f89873e7bc684e7472960
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-11-22 14:18:37 +0100

    fikser fejl efter Devise...

--- db/migrate/20250919081752_add_password_digest_to_users.rb
+++ db/migrate/20250919081752_add_password_digest_to_users.rb
@@ -1,7 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[8.0]
   def change
-    unless column_exists?(:users, :password_digest)
-      add_column :users, :password_digest, :string
-    end
+    add_column :users, :password_digest, :string unless column_exists?(:users, :password_digest)
   end
 end
