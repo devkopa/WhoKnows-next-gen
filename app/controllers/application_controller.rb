61dc01bc6e4708250f4e4903958a74be10d0946b
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-11-22 10:51:06 +0100

    lavet opgaven 'security breach'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :force_password_reset_if_needed
+
+  private
+
+  def force_password_reset_if_needed
+    return unless user_signed_in?
+    return unless current_user.force_password_reset?
+
+    # Undgå at låse API'et hvis du har en mobil/webapp
+    return if request.format.json?
+
+    # Undgå redirect-loop på password-siden
+    allowed_paths = [
+      change_password_path,
+      update_password_path,
+      destroy_user_session_path  # tillad logout
+    ]
+
+    return if allowed_paths.include?(request.path)
+
+    redirect_to change_password_path, alert: "Du skal ændre din adgangskode pga. et sikkerhedsbrud."
+  end
 end
