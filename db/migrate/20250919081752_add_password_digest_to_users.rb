ba14096d53f785e47a2bf2b2b13def98740f10a8
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-11-30 01:13:34 +0100

    fjernet .exe fra nogle filer

--- db/migrate/20250919081752_add_password_digest_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :password_digest, :string unless column_exists?(:users, :password_digest)
-  end
-end
--- db/migrate/20251127100000_add_last_login_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddLastLoginToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :last_login, :datetime
-    add_index :users, :last_login
-  end
-end
