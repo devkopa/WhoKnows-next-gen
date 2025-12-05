edeb3547f69dbad2e2d3a933492f27bb9455e441
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-12-05 20:46:14 +0100

    sidste gang for i dag...

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ abort("The Rails environment is running in production mode!") if ENV['RAILS_ENV'
 # --- SIMPLECOV MUST BE CONFIGURED BEFORE ANY CODE IS LOADED ---
 require 'simplecov'
 require 'simplecov-json'
+require 'pathname'
+
+SimpleCov.root(File.expand_path('..', __dir__))
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
@@ -17,9 +20,19 @@ SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/'
+
+  # Make file paths relative and convert backslashes to forward slashes
+  at_exit do
+    result = SimpleCov.result
+    result.files.each do |file|
+      relative_path = Pathname.new(file.filename).relative_path_from(Pathname.new(SimpleCov.root)).to_s
+      file.instance_variable_set(:@filename, relative_path.gsub('\\', '/'))
+    end
+    result.format!
+  end
 end
 
-# --- REQUIRE STATEMENTS FOR COVERAGE / TOOLS (must be before app code) ---
+# --- REQUIRE STATEMENTS FOR COVERAGE / TOOLS ---
 require 'selenium/webdriver'
 require_relative '../config/environment'
 require 'rspec/rails'
@@ -28,10 +41,8 @@ require 'spec_helper'
 # Autoload support files
 Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
-# Abort if somehow the environment is not test
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-# Capybara custom driver
 Capybara.register_driver :edge do |app|
   Capybara::Selenium::Driver.new(
     app,
@@ -40,33 +51,24 @@ Capybara.register_driver :edge do |app|
   )
 end
 
-# ActiveRecord schema checks
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
-# RSpec config
 RSpec.configure do |config|
   config.fixture_paths = [ Rails.root.join('spec/fixtures') ]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 
-  # Ensure system specs use the Edge driver
   config.before(:each, type: :system) do
-    driven_by :edge, screen_size: [ 1400, 1400 ]
+    driven_by :edge, screen_size: [1400, 1400]
   end
 
-  # IMPORTANT: make request specs use a known host to avoid HostAuthorization blocking.
-  # Rails/RSpec uses test.host by default; ensure request specs use it explicitly.
   config.before(:each, type: :request) do
-    # host! is provided by Rails test helpers and sets the Host header for requests
     host! "test.host"
   end
-
-  # If you prefer localhost instead, uncomment the next line:
-  # config.before(:each, type: :request) { host! "localhost" }
 
   config.filter_rails_from_backtrace!
 end
