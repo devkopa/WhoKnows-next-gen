3584dc31708b27f1efd151acf4933ae398948a9a
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-12-12 10:56:57 +0100

    Use SimpleCov JSON formatter with relative paths for SonarQube

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,13 +6,16 @@ abort("The Rails environment is running in production mode!") if ENV['RAILS_ENV'
 
 # --- SIMPLECOV MUST BE CONFIGURED BEFORE ANY CODE IS LOADED ---
 require 'simplecov'
+require 'simplecov-json'
 require 'json'
 require 'pathname'
 
 SimpleCov.root(File.expand_path('..', __dir__))
 
-# Use only HTML formatter - SimpleCov automatically generates .resultset.json
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter
+])
 
 SimpleCov.start 'rails' do
   add_filter '/bin/'
@@ -21,40 +24,26 @@ SimpleCov.start 'rails' do
   add_filter '/test/'
   add_filter '/config/'
 
-  # Post-process .resultset.json to convert absolute paths to relative paths for SonarQube
+  # Post-process coverage.json to use relative paths for SonarQube
   at_exit do
     SimpleCov.result.format!
 
-    resultset_path = File.join(SimpleCov.coverage_dir, '.resultset.json')
-    if File.exist?(resultset_path)
-      data = JSON.parse(File.read(resultset_path))
+    coverage_json_path = File.join(SimpleCov.coverage_dir, 'coverage.json')
+    if File.exist?(coverage_json_path)
+      data = JSON.parse(File.read(coverage_json_path))
       root_path = SimpleCov.root
 
-      # Convert absolute paths to relative paths in the coverage data
-      data.each do |_command_name, command_data|
-        next unless command_data['coverage']
-
-        new_coverage = {}
-        command_data['coverage'].each do |file_path, coverage_data|
-          relative_path = Pathname.new(file_path)
-                                  .relative_path_from(Pathname.new(root_path))
-                                  .to_s
-                                  .gsub('\\', '/')
-
-          # SimpleCov uses different formats - normalize to just line array for SonarQube
-          if coverage_data.is_a?(Hash) && coverage_data['lines']
-            # If it's a hash with 'lines', extract just the lines array
-            new_coverage[relative_path] = coverage_data['lines']
-          else
-            # Otherwise use as-is (should already be an array)
-            new_coverage[relative_path] = coverage_data
-          end
-        end
-
-        command_data['coverage'] = new_coverage
+      # Convert absolute paths to relative paths
+      data['files'].each do |file_data|
+        absolute_path = file_data['filename']
+        relative_path = Pathname.new(absolute_path)
+                                .relative_path_from(Pathname.new(root_path))
+                                .to_s
+                                .gsub('\\', '/')
+        file_data['filename'] = relative_path
       end
 
-      File.write(resultset_path, JSON.pretty_generate(data))
+      File.write(coverage_json_path, JSON.pretty_generate(data))
     end
   end
 end
